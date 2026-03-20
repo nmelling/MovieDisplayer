a256621dca03bc262179f978de8c8c410d5e8a83
nmellinger <nicolas.mellinger@terascop.com>
2026-03-20 20:31:12 +0100

    base api call with circular loader

---
+++ app/components/moviesList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { TMDB } from "tmdb-ts";
+
+type Movies = Awaited<ReturnType<TMDB["movies"]["popular"]>>["results"];
+
+defineProps<{
+  movies: Movies
+}>();
+</script>
+
+<template>
+  <div class="container">
+    {{ movies }}
+  </div>
+</template>
+
+<style>
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
+import MoviesList from "~/components/moviesList.vue";
 
+const MOVIES_URL = "/api/movies/popular";
+const MOVIES_KEY = "movies";
+
+const page = ref(1);
+
+const { data, pending } = await useAsyncData(
+  `${MOVIES_KEY}_${page.value}`,
+  () => $fetch(MOVIES_URL, {
+    params: { page: page.value },
+  }),
+  {
+    watch: [page],
+  }
+);
 </script>
 
 <template>
-  <h1>Liste des films</h1>
+  <header class="flex items-center justify-center">
+    <h1 class="mx-2">
+      Liste des films
+    </h1>
+    <v-progress-circular
+      v-show="pending"
+      indeterminate
+      color="primary"
+      aria-label="movies list loading"
+    />
+  </header>
+  <v-container>
+    <MoviesList :movies="data?.results ?? []" />
+  </v-container>
 </template>
 
 <style>
