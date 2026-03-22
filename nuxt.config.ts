45c1c129b7d866e3bb365f310e9676f0d5d7ebc5
nmellinger <nicolas.mellinger@terascop.com>
2026-03-22 23:21:48 +0100

    pinia pesist module & vuelidate install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/a11y", "@pinia/nuxt", "@vueuse/nuxt", "@nuxt/eslint"],
+  modules: ["@nuxt/a11y", "@pinia/nuxt", "pinia-plugin-persistedstate/nuxt", "@vueuse/nuxt", "@nuxt/eslint"],
   ssr: true,
   devtools: { enabled: true },
   app: {
